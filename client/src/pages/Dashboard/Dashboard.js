804645661caa1e73ce04328de5a04d6afb572af0
gatekeeper <63681296+truittj@users.noreply.github.com>
2020-07-12 20:17:11 -0500

    Added basic functions to DashBoard.js

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Row, Container } from "../../components/Grid";
 import DeleteBtn from "../../components/DeleteBtn";
 import DashboardCard from "../../components/DashboardCard"
 
 function Dashboard () {
+
+    const [cards, setCards] = useState([])
+    const [cardObject, setCardObject] = useState({
+      label: "",
+      otherText: "",
+      cost: 0,
+      notes: ""
+    })
+
+    useEffect(() => {
+        loadCards()
+      }, [])
+
+      function loadBooks() {
+        API.getCardInfo()
+          .then(res => 
+            setBooks(res.data)
+          )
+          .catch(err => console.log(err));
+      };
+
+      function deleteCard(id) {
+        API.deleteCard(id)
+          .then(res => loadCards())
+          .catch(err => console.log(err));
+      }
+
+      function handleInputChange(event) {
+        const { name, value } = event.target;
+        setCardObject({...cardObject, [name]: value})
+      };
+
+      function handleFormSubmit(event) {
+        event.preventDefault();
+        if (cardObject.title && cardObject.otherText) {
+          API.saveBook({
+            label: formObject.label,
+            otherText: formObject.otherText,
+            cost: formObject.cost,
+            notes: formObject.notes
+          })
+            .then(() => setFormObject({
+              label: "",
+              otherText: "",
+              cost: 0,
+              notes: ""
+            }))
+            .then(() => loadBooks())
+            .catch(err => console.log(err));
+        }
+      };
 
 return (
 
