27535c56f22ee023c9cb8854725351ae5d4e5fd9
Alyssa Schmalenberger <a.schmalenberger@gmail.com>
2020-07-14 17:34:55 -0500

    individual card working for dashboard with dummy info still needs to have functional code and conditional logic for categories

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Nav from "./components/Nav";
 import CardPlanning from "./components/CardPlanning"
+import CardIndv from "./components/CardIndv"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Nav />
 
       <CardPlanning />
+      <CardIndv />
     </div>
   );
 }
---
+++ client/src/components/CardIndv/index.js
@@ -0,0 +1,56 @@
+import React from "react";
+ // @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+
+// import AirplanemodeActiveIcon from '@material-ui/icons/AirplanemodeActive';
+// import RestaurantIcon from '@material-ui/icons/Restaurant';
+import HotelIcon from '@material-ui/icons/Hotel';
+// import PhotoCameraIcon from '@material-ui/icons/PhotoCamera';
+// import AddCircleIcon from '@material-ui/icons/AddCircle';
+
+
+
+ // core components
+import GridItem from "../Grid2/GridItem.js";
+import GridContainer from "../Grid2/GridContainer.js";
+import Button from "../Button/Button.js";
+import Card from "../Card/Card.js";
+import CardHeader from "../Card/CardHeader.js";
+import CardBody from "../Card/CardBody.js";
+import CardFooter from "../Card/CardFooter.js";
+import CardIcon from "../Card/CardIcon.js";
+import styles from "../Styles/CardIndvStyles"
+
+const useStyles = makeStyles(styles);
+
+export default function CardIndv() {
+    const classes = useStyles();
+    return (
+        <div>
+            <GridContainer>
+                <GridItem xs={12} sm={6} md={3}>
+                    <Card>
+                        <CardHeader color="warning" stats icon>
+                        <CardIcon color="warning">
+                            <HotelIcon />
+                            {/* //will need to render card icons accringly */}
+                        </CardIcon>
+                        <p className={classes.cardCategory}>Lodging</p>
+                        </CardHeader>
+                        <CardBody>
+                            <p>Name: </p>
+                            <p>Date: </p>
+                            <p>Time: </p>
+                            <p>Locatiom: </p>
+                            <p>Notes: </p>
+                        </CardBody>
+                        <CardFooter stats>
+                            <Button>Edit</Button>
+                            <Button>Delete</Button>
+                        </CardFooter>
+                    </Card>
+                </GridItem>
+            </GridContainer>
+        </div>
+    );
+}
