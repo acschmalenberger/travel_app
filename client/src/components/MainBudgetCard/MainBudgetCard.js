458c8e3d44cdac461cb73136c3458fa2401fae14
gatekeeper <63681296+truittj@users.noreply.github.com>
2020-07-15 00:00:01 -0500

    There is a data-objectName that throws and error and when it is corrected, it breaks the code in different unperdicitable ways

--- client/src/components/MainBudgetCard/MainBudgetCard.js
+++ client/src/components/MainBudgetCard/MainBudgetCard.js
@@ -7,7 +7,7 @@ function MainBudgetCard (props) {
 return(
     <div className="card-deck">
         <div className="card">
-            <img src="..." className="card-img-top" alt="..." />
+            {/* <img src="..." className="card-img-top" alt="..." /> */}
         <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">This is the main budget card</p>
