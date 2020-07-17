762638aaffafc2a0f047a9890d2aa4cc36929f68
gatekeeper <63681296+truittj@users.noreply.github.com>
2020-07-17 14:28:14 -0500

    Merged successfully with Alyssa

--- client/src/components/auth/Signin.js
+++ client/src/components/auth/Signin.js
@@ -71,7 +71,7 @@ class Signin extends Component {
         const { classes } = this.props;
         const { from } = this.props.location.state || {
             from: {
-                pathname: '/Dashboard'
+                pathname: '/Start'
             }
         };
         const { redirectToReferrer } = this.state;
