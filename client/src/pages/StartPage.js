ed04caa42dff7d25876fd4cd334653eae37e0225
Derrick Chargois <36555200+dchargois23@users.noreply.github.com>
2020-07-19 13:43:10 -0500

    commit all changes from joel mergee

--- client/src/pages/StartPage.js
+++ client/src/pages/StartPage.js
@@ -9,13 +9,7 @@ import "./StartPageStyle.css"
 
 function StartPage() {
 
-<<<<<<< HEAD
-    const [cards, setCards] = useState({})
-
     const [budgetObject, setBudgetObject] = useState({
-=======
-        const [budgetObject, setBudgetObject] = useState({
->>>>>>> a2ec97164456089652ba6dbba52209e808289028
         Name: "Value",
         overallBudget: "",
         spentBudget: "",
@@ -23,25 +17,6 @@ function StartPage() {
         startDate: "",
         endDate: ""
     })
-
-<<<<<<< HEAD
-    useEffect(() => {
-        loadCards()
-    }, [])
-
-    function loadCards() {
-        API.getCards()
-            .then(res =>
-                setCards(res.data)
-            )
-            .catch(err => console.log(err));
-    };
-
-    function deleteCard(id) {
-        API.deleteCard(id)
-            .then(res => loadCards())
-            .catch(err => console.log(err));
-    }
 
     function handleChange(e) {
         const { name, value } = e.target;
@@ -53,69 +28,30 @@ function StartPage() {
 
     };
 
-    function handleFormSubmit(event) {
+    function handleTrip(event) {
         event.preventDefault();
-        let cardSubmitObj = event.target.getAttribute("data-objectname")
-        console.log(cards, cardSubmitObj)
-
-    };
-
-    function createCard() {
-        console.log(budgetObject);
-        API.saveCard(budgetObject);
+        API.createTrip(budgetObject)
+            .then(() => setBudgetObject({}))
+            .then(document.location.href = '/Plan')
+            .catch(err => console.log(err));
     }
 
     return (
-        <div>
-
-            <GridContainer>
-                <GetStarted
-                    name={budgetObject}
-                    handleInputChange={handleChange}
-                    objectName={"budgetObject"}
-                    value={budgetObject}
-                    info={setBudgetObject}
-                    execute={createCard}
-                    handleFormSubmit={handleFormSubmit}
-                />
-            </GridContainer>
-
-=======
-        function handleChange(e) {
-            const { name, value } = e.target;
-            const objectName = e.target.getAttribute("data-objectname");
-                switch (objectName) {
-                    case "budgetObject":
-                    return setBudgetObject({...budgetObject, [name]: value})
-                };
-
-                };
-
-        function handleTrip(event) {
-            event.preventDefault();
-                API.createTrip(budgetObject)
-                    .then(() => setBudgetObject({}))
-                    .then(document.location.href = '/Plan')
-                    .catch(err => console.log(err));
-            }
- 
-    return ( 
         <div className="startContainer">
             <div>
 
-            <GridContainer>
-                <GetStarted            
-                name={budgetObject}
-                handleInputChange={handleChange}
-                objectName={"budgetObject"}
-                value={budgetObject} 
-                info={setBudgetObject}
-                handleTrip={handleTrip}
-                />
-            </GridContainer>
+                <GridContainer>
+                    <GetStarted
+                        name={budgetObject}
+                        handleInputChange={handleChange}
+                        objectName={"budgetObject"}
+                        value={budgetObject}
+                        info={setBudgetObject}
+                        handleTrip={handleTrip}
+                    />
+                </GridContainer>
 
             </div>
->>>>>>> a2ec97164456089652ba6dbba52209e808289028
         </div>
 
     );
